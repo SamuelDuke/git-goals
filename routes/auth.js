44cc076c346a17d479198d5aab86ff806b05e8b2
SamuelDuke <samuelwinterton@gmail.com>
2019-10-18 14:16:27 -0600

    Added JWT Local Authentication.

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,18 @@
+const jwt = require('jsonwebtoken');
+const configMain = require('../config/main');
+
 const User = require('../data_models/user');
+
+const generateToken = (user) => {
+    return jwt.sign(user, configMain.jwtSecret, {
+        expiresIn: configMain.jwtExpiration // in seconds
+    });
+};
+
+const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+};
 
 exports.loginUser = (req, res, next) => {
     res.json({Data: "Login"})
@@ -7,13 +21,34 @@ exports.loginUser = (req, res, next) => {
 exports.registerUser = (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
 
-
-    User({ firstName, lastName, email, password })
+    User({ firstName, lastName, email: email.toLowerCase(), password })
         .save()
         .then(user => {
-        return res.json(user);
+        return res.json(user.infoToSend());
     })
 .catch(err => {
         return res.json({msg: "There was an error",error: err});
     })
+};
+
+exports.generateJWTLocal = (req, res, next) => {
+    const { email: email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({message: 'This post request requires the following in the body of the request: [password, email]'})
+    }
+
+    User.findOne({email: email.toLowerCase()}).exec()
+        .then(user => {
+            if (user === null) {
+                return res.status(400).json({error: 'That email is not registered.'});
+            }
+
+            if (user.validPassword(req.body.password)) {
+                const token = generateToken(user.infoToSend());
+                return res.status(200).json({token: token});
+            } else {
+                return res.status(400).json({error: 'That is the wrong password.'});
+            }
+        }).then(null, err => { return next(err); });
 };
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+//const mongoose = require('mongoose');
+const User = require('../data_models/user');
+
+exports.getMe = (req, res, next) => {
+    User.findOne({_id: req.user._id})
+        //.populate('groups')
+        .exec()
+        .then(me => {
+            return res.status(200).json(me.infoToSend());
+        }).catch(err => {
+        res.status(400).json({error: 'There was an error'});
+        return next(err);
+    })
+};
